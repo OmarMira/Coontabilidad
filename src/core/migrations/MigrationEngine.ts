5e8bdccfffa7b28a24710b79ee7fb3b673ce2c8e
OmarMira <55993765+OmarMira@users.noreply.github.com>
2026-03-12 14:02:49 -0400

    Fix 032: agregar county_code a florida_tax_rates

--- src/core/migrations/MigrationEngine.ts
+++ src/core/migrations/MigrationEngine.ts
@@ -28,6 +28,7 @@ import { AddAccountNumberMigration } from './list/028_add_account_number';
 import { AddDetailTypeMigration } from './list/029_add_detail_type';
 import { TaxRatesConfigMigration } from './list/030_tax_rates_config';
 import { UsersAndCompanyDataMigration } from './list/031_users_and_company_data';
+import { AddCountyCodeToFloridaTaxRatesMigration } from './list/032_add_county_code_to_florida_tax_rates';
 
 export interface Migration {
     version: number;
@@ -72,7 +73,8 @@ export class MigrationEngine {
         AddAccountNumberMigration,             // v28
         AddDetailTypeMigration,                // v29
         TaxRatesConfigMigration,                 // v30
-        UsersAndCompanyDataMigration             // v31
+        UsersAndCompanyDataMigration,            // v31
+        AddCountyCodeToFloridaTaxRatesMigration  // v32
     ];
 
     private constructor() { }
